b861543f46c9d6a9676d3240245b0cfae9cb255e
bricktrooper <kylepinto@yahoo.ca>
2022-03-07 00:47:17 -0500

    Receive RGB values over UART

--- firmware/main.c
+++ firmware/main.c
@@ -1,10 +1,26 @@
 #include <xc.h>
 #include <pic16f690.h>
+#include <stdio.h>
 
 #include "init.h"
 #include "led.h"
 #include "uart.h"
 #include "pwm.h"
+
+/* The PIC will only receive 3 bytes
+ * at a time.  These bytes will be
+ * interpreted as brightness values
+ * for red, green, and blue. Therefore,
+ * values should be sent in this order. */
+
+enum Colour
+{
+	RED,
+	BLUE,
+	GREEN,
+
+	NUM_COLOURS
+};
 
 void main(void)
 {
@@ -13,12 +29,14 @@ void main(void)
 	uart_init();
 	pwm_init();
 
-	pwm_green(255);
-	pwm_red(255);
-	pwm_blue(255);
+	unsigned char rgb [NUM_COLOURS];
 
 	while (1)
 	{
-		uart_echo();
+		uart_receive(rgb, NUM_COLOURS);
+
+		pwm_green(rgb[RED]);
+		pwm_red(rgb[GREEN]);
+		pwm_blue(rgb[BLUE]);
 	}
 }
